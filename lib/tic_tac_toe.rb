1530f9489486c3e253f8681a7671d0021f7ef528
Matthew DePaso <matthewhdepaso@gmail.com>
2019-10-06 22:45:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,43 +1,43 @@
-# WIN_COMBINATIONS = [
-# [0,1,2], [3,4,5], [6,7,8], #row wins
-# [0,3,6],[1,4,7], [2,5,8], #column wins
-# [0,4,8],[6,4,2], #Diagnol Wins
+WIN_COMBINATIONS = [
+[0,1,2], [3,4,5], [6,7,8], #row wins
+[0,3,6],[1,4,7], [2,5,8], #column wins
+[0,4,8],[6,4,2], #Diagnol Wins
 
-# ]
+]
 
-# def display_board(board)
-#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-#   puts "-----------"
-#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-#   puts "-----------"
-#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-# end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-# # board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-# display_board(board)
+display_board(board)
 
-# def input_to_index(user_input)
-#   user_input = user_input.to_i-1
-# end
+def input_to_index(user_input)
+  user_input = user_input.to_i-1
+end
 
-# def move(board,index,char)
-#   board[index] = char
-# end
+def move(board,index,char)
+  board[index] = char
+end
 
 
-# # This checks if a space is taken. By checking is the space has and X or and O in it.
-# def position_taken?(board, position)
-#   if board[position] == "X" || board[position] == "O"
-#     return true
-#   else
-#     return false
-#   end
-# end
+# This checks if a space is taken. By checking is the space has and X or and O in it.
+def position_taken?(board, position)
+  if board[position] == "X" || board[position] == "O"
+    return true
+  else
+    return false
+  end
+end
 
-# def valid_move(board, index)
-#   if position_taken?
+def valid_move(board, index)
+  if position_taken?
   
-# end
+end
 
 
