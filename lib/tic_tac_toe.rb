e7b73eacba4ecc18699632dfb70a26fa20d1291b
Matthew DePaso <matthewhdepaso@gmail.com>
2019-10-06 22:47:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,9 +35,9 @@ def position_taken?(board, position)
   end
 end
 
-# def valid_move(board, index)
-#   if position_taken?
-  
-# end
+def valid_move(board, index)
+   if position_taken?
+  end
+end
 
 
